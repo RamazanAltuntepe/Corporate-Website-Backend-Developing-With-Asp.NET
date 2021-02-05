437ef1e2cb13d9c7a51e20fb26236e72c6b61329
Ramazan Altuntepe <69468753+RamazanAltuntepe@users.noreply.github.com>
2021-02-06 02:37:17 +0300

    Adding Pages

---
+++ Narail/Narail/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Narail.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public PartialViewResult BlogDetail()
+        {
+            return PartialView("BlogDetailContent");
+
+        }
+    }
+}
