30b8a9c5d1b36da028fa4187663d2bb4e8b1485d
Ramazan Altuntepe <69468753+RamazanAltuntepe@users.noreply.github.com>
2021-02-09 01:30:40 +0300

    Contact Form Mail Sender Added
    Mail sender automation with NET.Mail library.

--- Narail/Narail/Controllers/ContactController.cs
+++ Narail/Narail/Controllers/ContactController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Narail.Models;
 
@@ -15,8 +16,29 @@ namespace Narail.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Contact(ContactFormModel contactForm)
+        public IActionResult Contact(ContactFormModel contactModel)
         {
+            MailMessage mail  = new MailMessage();
+            mail.To.Add("ramazanalltuntepe@gmail.com");
+            mail.From = new MailAddress("ramazanalltuntepe@gmail.com");
+            mail.Subject = contactModel.ContactSubject;
+            mail.Body = contactModel.ContactName + contactModel.ContactMessage + contactModel.ContactPhone + contactModel.ContactPhone;
+            mail.IsBodyHtml = true;
+            
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Port = 587;
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.EnableSsl = true;
+            try
+            {
+                smtpClient.Send(mail);
+                TempData["Message"] = "Gönderim Başarılı!.";
+            }
+            catch (Exception e)
+            {
+                TempData["Message"] = "Gönderim Başarısız." + e.Message;
+            }
+            
             return View();
         }
 
