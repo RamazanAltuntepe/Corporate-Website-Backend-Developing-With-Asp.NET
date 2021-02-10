7e185c5ee2411f8b886eed92d998a15bb73360ca
Ramazan Altuntepe <69468753+RamazanAltuntepe@users.noreply.github.com>
2021-02-10 21:07:47 +0300

    Entity Data Models
    Created SQL datebase and tables, and added entity framework models.

---
+++ Narail/Narail/Models/EFDataModel/BlogContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Narail.Models.EFDataModel
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blog { get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Narail/Narail/Models/EFDataModel/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Narail.Models.EFDataModel
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
